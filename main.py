d9cd69064066c1d2e8f56d684cd44bb6d6691842
Yuji Saikai <saikai@me.com>
2016-03-27 23:18:46 -0500

    sellers, matching matrix, etc.

--- main.py
+++ main.py
@@ -13,8 +13,12 @@ Notes
 Try to match singular, plural, upper, & lower cases.
 e.g. Wolf(ves) -> Seller(s), Sheep -> Buyer and Buyers (unfortunate!)
 
-[Killed]
+[Removed]
 Grass, reproduction,
+
+[Activation]
+1. Sellers post prices
+2. Buyers learn the prices, choose one, and buy one unit
 
 '''
 
@@ -26,308 +30,247 @@ from mesa.space import MultiGrid
 from mesa.time import RandomActivation
 from mesa.datacollection import DataCollector
 
-# from RandomWalk import RandomWalker
 
-
-# class WolfSheepPredation(Model):
 class Trade(Model):
-    height = 20
-    width = 20
+  height = 20
+  width = 20
 
-    # initial_sheep = 100
-    # initial_wolves = 50
-    initial_buyers = 100
-    initial_sellers = 50
+  ini_buyers = 100
+  ini_sellers = 50
 
-    # sheep_reproduce = 0.04
-    # wolf_reproduce = 0.05
-	#
-    # wolf_gain_from_food = 20
-	#
-    # grass = False
-    # grass_regrowth_time = 30
-    # sheep_gain_from_food = 4
+  # sheep_reproduce = 0.04
+  # wolf_reproduce = 0.05
+  #
+  # wolf_gain_from_food = 20
+  #
+  # grass = False
+  # grass_regrowth_time = 30
+  # sheep_gain_from_food = 4
 
-    verbose = False # Print-monitoring
+  verbose = False # Print-monitoring
 
-    # def __init__(self, height=20, width=20,
-    #              initial_sheep=100, initial_wolves=50,
-    #              sheep_reproduce=0.04, wolf_reproduce=0.05,
-    #              wolf_gain_from_food=20,
-    #              grass=False, grass_regrowth_time=30, sheep_gain_from_food=4):
-    def __init__(self, height=20, width=20,
-                 initial_buyers=100, initial_sellers=50):
-        '''
-        Create a new LF model with the given parameters.
+  def __init__(self, height=20, width=20,
+         ini_buyers=100, ini_sellers=50):
+    '''
+    Create a new LF model with the given parameters.
 
-        Args:
-            initial_buyers: Number of buyers to start with
-            initial_sellers: Number of seller to start with
-        '''
+    Args:
+      ini_buyers: Number of buyers to start with
+      ini_sellers: Number of seller to start with
+    '''
 
-        # Set parameters
-        self.height = height
-        self.width = width
-        self.initial_buyers = initial_buyers
-        self.initial_sellers = initial_sellers
-        # self.sheep_reproduce = sheep_reproduce
-        # self.wolf_reproduce = wolf_reproduce
-        # self.wolf_gain_from_food = wolf_gain_from_food
-        # self.grass = grass
-        # self.grass_regrowth_time = grass_regrowth_time
-        # self.sheep_gain_from_food = sheep_gain_from_food
+    # Set parameters
+    self.height = height
+    self.width = width
+    self.ini_buyers = ini_buyers
+    self.ini_sellers = ini_sellers
+    # self.sheep_reproduce = sheep_reproduce
+    # self.wolf_reproduce = wolf_reproduce
+    # self.wolf_gain_from_food = wolf_gain_from_food
+    # self.grass = grass
+    # self.grass_regrowth_time = grass_regrowth_time
+    # self.sheep_gain_from_food = sheep_gain_from_food
 
-        self.schedule = RandomActivationByBreed(self)
-        self.grid = MultiGrid(self.height, self.width, torus=True)
-        # self.datacollector = DataCollector(
-        #     {"Wolves": lambda m: m.schedule.get_breed_count(Wolf),
-        #     "Sheep": lambda m: m.schedule.get_breed_count(Sheep)})
-        self.datacollector = DataCollector(
-            {"Sellers": lambda m: m.schedule.get_breed_count(Seller),
-            "Buyers": lambda m: m.schedule.get_breed_count(Buyer)})
-
-        # Create buyers:
-        for i in range(self.initial_buyers):
-            x = random.randrange(self.width)
-            y = random.randrange(self.height)
-            # energy = random.randrange(2 * self.sheep_gain_from_food)
-            buyer = Buyer(self.grid, (x, y), True, energy) # energy?
-            self.grid.place_agent(buyer, (x, y))
-            self.schedule.add(buyer)
-
-        # Create sellers
-        for i in range(self.initial_sellers):
-            x = random.randrange(self.width)
-            y = random.randrange(self.height)
-            energy = random.randrange(2 * self.wolf_gain_from_food)
-            seller = Seller(self.grid, (x, y), True, energy)
-            self.grid.place_agent(seller, (x, y))
-            self.schedule.add(seller)
-
-        # # Create grass patches
-        # if self.grass:
-        #     for agent, x, y in self.grid.coord_iter():
-		#
-        #         fully_grown = random.choice([True, False])
-		#
-        #         if fully_grown:
-        #             countdown = self.grass_regrowth_time
-        #         else:
-        #             countdown = random.randrange(self.grass_regrowth_time)
-		#
-        #         patch = GrassPatch(fully_grown, countdown)
-        #         self.grid.place_agent(patch, (x, y))
-        #         self.schedule.add(patch)
-
-        self.running = True
-
-    def step(self):
-        self.schedule.step()
-        self.datacollector.collect(self)
-        if self.verbose:
-            print([self.schedule.time,
-                self.schedule.get_breed_count(Seller),
-                self.schedule.get_breed_count(Buyer)])
-
-    def run_model(self, step_count=200):
-        if self.verbose:
-            print('Initial number sellers: ',
-                self.schedule.get_breed_count(Seller))
-            print('Initial number buyers: ',
-                self.schedule.get_breed_count(Buyer))
-
-        for i in range(step_count):
-            self.step()
-
-        if self.verbose:
-            print('')
-            print('Final number sellers: ',
-                self.schedule.get_breed_count(Seller))
-            print('Final number buyers: ',
-                self.schedule.get_breed_count(Buyer))
-
-class Buyer(RandomWalker, Agent):
-    energy = None
-
-    def __init__(self, grid, pos, moore, energy=None):
-        super().__init__(grid, pos, moore)
-        self.energy = energy
-
-    def step(self, model):
-        '''
-        A model step. Move, then eat grass and reproduce.
-        '''
-        self.random_move()
-        living = True
-
-        if model.grass:
-            # Reduce energy
-            self.energy -= 1
-
-            # If there is grass available, eat it
-            this_cell = model.grid.get_cell_list_contents([self.pos])
-            grass_patch = [obj for obj in this_cell
-                           if isinstance(obj, GrassPatch)][0]
-            if grass_patch.fully_grown:
-                self.energy += model.sheep_gain_from_food
-                grass_patch.fully_grown = False
-
-            # Death
-            if self.energy < 0:
-                model.grid._remove_agent(self.pos, self)
-                model.schedule.remove(self)
-                living = False
-
-        if living and random.random() < model.sheep_reproduce:
-            # Create a new sheep:
-            if model.grass:
-                self.energy /= 2
-            lamb = Sheep(self.grid, self.pos, self.moore, self.energy)
-            model.grid.place_agent(lamb, self.pos)
-            model.schedule.add(lamb)
+    self.schedule = RandomActivationByBreed(self)
+    self.grid = MultiGrid(self.height, self.width, torus=True)
+    # self.datacollector = DataCollector(
+    #   {"Wolves": lambda m: m.schedule.get_breed_count(Wolf),
+    #   "Sheep": lambda m: m.schedule.get_breed_count(Sheep)})
+    self.datacollector = DataCollector(
+      {"Sellers": lambda m: m.schedule.get_breed_count(Seller),
+      "Buyers": lambda m: m.schedule.get_breed_count(Buyer)})
 
 
-class Seller(RandomWalker, Agent):
+    '''
+    Generate a matching matrix
+
+      (for now) just random 0 & 1
+      Sellers are on rows. Buyers on columns.
+      Wal-Mart has 1s for every buyers (correct?)
+    '''
+    match = np.random.randint(2, size=(ini_sellers-1, ini_buyers))
+    match = np.append(match, [np.ones(ini_buyers,dtype=np.int)], axis=0)
+
+
+    # Create buyers:
+    for i in range(self.ini_buyers):
+      x = random.randrange(self.width)
+      y = random.randrange(self.height)
+      # energy = random.randrange(2 * self.sheep_gain_from_food)
+      buyer = Buyer(self.grid, (x, y), True, energy) # energy?
+      self.grid.place_agent(buyer, (x, y))
+      self.schedule.add(buyer)
+
+    # Create sellers
+    for i in range(self.ini_sellers):
+      x = random.randrange(self.width)
+      y = random.randrange(self.height)
+      energy = random.randrange(2 * self.wolf_gain_from_food)
+      seller = Seller(self.grid, (x, y), True, energy)
+      self.grid.place_agent(seller, (x, y))
+      self.schedule.add(seller)
+
+    self.running = True
+
+  def step(self):
+    self.schedule.step()
+    self.datacollector.collect(self)
+    if self.verbose:
+      print([self.schedule.time,
+        self.schedule.get_breed_count(Seller),
+        self.schedule.get_breed_count(Buyer)])
+
+  def run_model(self, step_count=200):
+    if self.verbose:
+      print('Initial number sellers: ',
+        self.schedule.get_breed_count(Seller))
+      print('Initial number buyers: ',
+        self.schedule.get_breed_count(Buyer))
+
+    for i in range(step_count):
+      self.step()
+
+    if self.verbose:
+      print('')
+      print('Final number sellers: ',
+        self.schedule.get_breed_count(Seller))
+      print('Final number buyers: ',
+        self.schedule.get_breed_count(Buyer))
+
+
+class Buyer(Agent):
 '''
-For now, borrow 'energy' to control their mortality.
-A special seller 'W' represetns a conventional producer. Immortal.
+income: for wealth
 '''
 
-    energy = None
+  income = None # don't know how it is used...
 
-    def __init__(self, grid, pos, moore, energy, price):
-        super().__init__(grid, pos, moore)
-        self.energy = energy
-		# initial price vector
-		self.price = price
+  def __init__(self, grid, pos, moore, income):
+    self.grid = grid
+    self.pos = pos
+    self.moore = moore
+    self.income = income
 
-    def step(self, model):
-		# If selling below the threshold, reduce 1
-		if...
-			self.energy -= 1
+  def step(self, model):
+    if model.grass:
 
-        # # If there are sheep present, eat one
-        # x, y = self.pos
-        # this_cell = model.grid.get_cell_list_contents([self.pos])
-        # sheep = [obj for obj in this_cell if isinstance(obj, Sheep)]
-        # if len(sheep) > 0:
-        #     sheep_to_eat = random.choice(sheep)
-        #     self.energy += model.wolf_gain_from_food
-		#
-        #     # Kill the sheep
-        #     model.grid._remove_agent(self.pos, sheep_to_eat)
-        #     model.schedule.remove(sheep_to_eat)
+      # Reduce energy
+      self.energy -= 1
 
-        # Death
-        if self.energy < 0:
-            model.grid._remove_agent(self.pos, self)
-            model.schedule.remove(self)
-        # else:
-        #     if random.random() < model.wolf_reproduce:
-        #         # Create a new wolf cub
-        #         self.energy /= 2
-        #         cub = Wolf(self.grid, self.pos, self.moore, self.energy)
-        #         model.grid.place_agent(cub, cub.pos)
-        #         model.schedule.add(cub)
+      # If there is grass available, eat it
+      this_cell = model.grid.get_cell_list_contents([self.pos])
+      grass_patch = [obj for obj in this_cell
+               if isinstance(obj, GrassPatch)][0]
+      if grass_patch.fully_grown:
+        self.energy += model.sheep_gain_from_food
+        grass_patch.fully_grown = False
 
 
 
-# class GrassPatch(Agent):
-#     '''
-#     A patch of grass that grows at a fixed rate and it is eaten by sheep
-#     '''
-#
-#     def __init__(self, fully_grown, countdown):
-#         '''
-#         Creates a new patch of grass
-#
-#         Args:
-#             grown: (boolean) Whether the patch of grass is fully grown or not
-#             countdown: Time for the patch of grass to be fully grown again
-#         '''
-#         self.fully_grown = fully_grown
-#         self.countdown = countdown
-#
-#     def step(self, model):
-#         if not self.fully_grown:
-#             if self.countdown <= 0:
-#                 # Set as fully grown
-#                 self.fully_grown = True
-#                 self.countdown = model.grass_regrowth_time
-#             else:
-#                 self.countdown -= 1
-#
+class Seller(Agent):
+'''
+cash: for liquidity level. analogue of energy.
+w: boolean for conventional producer, who is immortal.
+'''
+
+  cash = None # don't know how it is used...
+
+  def __init__(self, grid, pos, moore, cash, costs, price, w=False):
+    self.grid = grid
+    self.pos = pos
+    self.moore = moore
+    self.cash = cash
+    self.costs = costs # fixed costs
+    self.price = price
+    self.w = w
+
+  def step(self, model):
+    # (if not w) Cash changes by sales - the fixed costs
+    if w==False:
+      self.cash = # need to get the sales at the previous period
+
+    # Insolvency
+    if self.cash < 0:
+      model.grid._remove_agent(self.pos, self)
+      model.schedule.remove(self)
+
+    # Post a new price
+    else:
+      # For now, it is fixed
+    #   self.price =
+
+
 
 class RandomActivationByBreed(RandomActivation):
+  '''
+  A scheduler which activates each type of agent once per step, in random
+  order, with the order reshuffled every step.
+
+  This is equivalent to the NetLogo 'ask breed...' and is generally the
+  default behavior for an ABM.
+
+  Assumes that all agents have a step(model) method.
+  '''
+  agents_by_breed = defaultdict(list)
+
+  def __init__(self, model):
+    super().__init__(model)
+    self.agents_by_breed = defaultdict(list)
+
+  def add(self, agent):
     '''
-    A scheduler which activates each type of agent once per step, in random
-    order, with the order reshuffled every step.
+    Add an Agent object to the schedule
 
-    This is equivalent to the NetLogo 'ask breed...' and is generally the
-    default behavior for an ABM.
-
-    Assumes that all agents have a step(model) method.
+    Args:
+      agent: An Agent to be added to the schedule.
     '''
-    agents_by_breed = defaultdict(list)
 
-    def __init__(self, model):
-        super().__init__(model)
-        self.agents_by_breed = defaultdict(list)
+    self.agents.append(agent)
+    agent_class = type(agent)
+    self.agents_by_breed[agent_class].append(agent)
 
-    def add(self, agent):
-        '''
-        Add an Agent object to the schedule
+  def remove(self, agent):
+    '''
+    Remove all instances of a given agent from the schedule.
+    '''
 
-        Args:
-            agent: An Agent to be added to the schedule.
-        '''
+    while agent in self.agents:
+      self.agents.remove(agent)
 
-        self.agents.append(agent)
-        agent_class = type(agent)
-        self.agents_by_breed[agent_class].append(agent)
+    agent_class = type(agent)
+    while agent in self.agents_by_breed[agent_class]:
+      self.agents_by_breed[agent_class].remove(agent)
 
-    def remove(self, agent):
-        '''
-        Remove all instances of a given agent from the schedule.
-        '''
+  def step(self, by_breed=True):
+    '''
+    Executes the step of each agent breed, one at a time, in random order.
 
-        while agent in self.agents:
-            self.agents.remove(agent)
+    Args:
+      by_breed: If True, run all agents of a single breed before running
+            the next one.
+    '''
+    if by_breed:
+      for agent_class in  self.agents_by_breed:
+        self.step_breed(agent_class)
+      self.steps += 1
+      self.time += 1
+    else:
+      super().step()
 
-        agent_class = type(agent)
-        while agent in self.agents_by_breed[agent_class]:
-            self.agents_by_breed[agent_class].remove(agent)
+  def step_breed(self, breed):
+    '''
+    Shuffle order and run all agents of a given breed.
 
-    def step(self, by_breed=True):
-        '''
-        Executes the step of each agent breed, one at a time, in random order.
+    Args:
+      breed: Class object of the breed to run.
+    '''
+    agents = self.agents_by_breed[breed]
+    random.shuffle(agents)
+    for agent in agents:
+      agent.step(self.model)
 
-        Args:
-            by_breed: If True, run all agents of a single breed before running
-                      the next one.
-        '''
-        if by_breed:
-            for agent_class in  self.agents_by_breed:
-                self.step_breed(agent_class)
-            self.steps += 1
-            self.time += 1
-        else:
-            super().step()
-
-    def step_breed(self, breed):
-        '''
-        Shuffle order and run all agents of a given breed.
-
-        Args:
-            breed: Class object of the breed to run.
-        '''
-        agents = self.agents_by_breed[breed]
-        random.shuffle(agents)
-        for agent in agents:
-            agent.step(self.model)
-
-    def get_breed_count(self, breed_class):
-        '''
-        Returns the current number of agents of certain breed in the queue.
-        '''
-        return len(self.agents_by_breed[breed_class])
+  def get_breed_count(self, breed_class):
+    '''
+    Returns the current number of agents of certain breed in the queue.
+    '''
+    return len(self.agents_by_breed[breed_class])
