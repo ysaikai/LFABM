5fba6e712851144b2d4c61d0b9a5be116f617e0e
Yuji Saikai <saikai@me.com>
2016-04-03 22:44:46 -0500

    add a separate color for Wal-Mart

--- server.py
+++ server.py
@@ -2,27 +2,39 @@ from main import Seller, Buyer, Trade
 from mesa.visualization.ModularVisualization import ModularServer
 from mesa.visualization.modules import CanvasGrid, ChartModule
 
+color_buyer = "#666666" # grey
+color_seller = "#339900" # green
+color_WM = "#990000" # Wal-Mart red
+modelname = "LFABM"
+
 def LFABM_portrayal(agent):
-    if agent is None:
-        return
+  if agent is None: return
 
-    portrayal = {"Shape": "circle", "Filled": "true"}
+  portrayal = {"Shape": "circle", "Filled": "true"}
+  '''
+  The higher layer is placed on top of the lower. So, the lower should have
+  a smaller portrayal["r"] (the size of shape) so that it can be seen.
+  '''
+  if type(agent) is Buyer:
+    portrayal["Color"] = color_buyer
+    portrayal["r"] = 0.8
+    portrayal["Layer"] = 1
 
-    if type(agent) is Buyer:
-        portrayal["Color"] = "#666666"
-        portrayal["r"] = 0.8
-        portrayal["Layer"] = 1
+  elif type(agent) is Seller:
+    if agent.w:
+      portrayal["Color"] = color_WM
+    else:
+      portrayal["Color"] = color_seller
+    portrayal["r"] = 0.5
+    portrayal["Layer"] = 2
 
-    elif type(agent) is Seller:
-        portrayal["Color"] = "#AA0000"
-        portrayal["r"] = 0.5
-        portrayal["Layer"] = 2
-
-    return portrayal
+  return portrayal
 
 canvas_element = CanvasGrid(LFABM_portrayal, 20, 20, 500, 500)
-chart_element = ChartModule([{"Label": "Sellers", "Color": "#AA0000"},
-    {"Label": "Buyers", "Color": "#666666"}])
+chart_element = ChartModule([
+  {"Label": "Sellers", "Color": color_seller},
+  {"Label": "Wal-Mart", "Color": color_WM},
+  {"Label": "Buyers", "Color": color_buyer}])
 
-server = ModularServer(Trade, [canvas_element, chart_element], "main")
+server = ModularServer(Trade, [canvas_element, chart_element], modelname)
 server.launch()
