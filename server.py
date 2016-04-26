538b9ca8dd80bedbfbe39acf763b0016c8829892
Yuji Saikai <saikai@me.com>
2016-04-26 17:26:08 -0500

    clean up
    We definitely do NOT understand how it is working...

--- server.py
+++ server.py
@@ -1,54 +1,44 @@
-# from main import Seller, Buyer, Trade
-import main
+from main import Seller, Buyer, Trade
 from mesa.visualization.ModularVisualization import ModularServer
 from mesa.visualization.modules import CanvasGrid, ChartModule
 
-'''Does this fix the cash balance issue?'''
-import importlib
-importlib.reload(main)
-
 color_buyer = "#f5f5f5" # very light grey
-color_seller = "#339900" # green
+color_seller = "#00FF00"
 price_color_on = 1 # display seller price in their color
-color_WM = "#990000" # Wal-Mart red
+color_WM = "#FF0000"
 modelname = "LFABM"
 
 def LFABM_portrayal(agent):
-  if agent is None: return
+  if (agent is None or type(agent) is Buyer): return
 
   portrayal = {"Shape": "circle", "Filled": "true"}
   '''
   The higher layer is placed on top of the lower. So, the lower should have
   a bigger portrayal["r"] (the size of shape) so that it can be seen.
   '''
-  # The following shows only the static color reflecting the initial prices.
-  # If we want to dynamically change the colors, need to go thru the scheduler.
-  if type(agent) is main.Seller:
-    if agent.w:
-      portrayal["Color"] = color_WM
-    elif (price_color_on):
-      val = 255-int(255*(agent.price/2))
-      price_color = '#'+str(val)+'FF'+str(val)   # Lighter = low price, darker = high price
-      portrayal["Color"] = price_color
-    else:
-      portrayal["Color"] = color_seller
-    portrayal["r"] = 0.5
-    portrayal["Layer"] = 2
+  # if type(agent) is Seller:
+  portrayal["Color"] = color_seller
+  if agent.w:
+    portrayal["Color"] = color_WM
+  elif (price_color_on):
+    val = 255-int(255*(agent.price/3))
+    # Lighter = low price, darker = high price
+    price_color = '#'+str(val)+'FF'+str(val)
+    portrayal["Color"] = price_color
 
-  elif type(agent) is main.Buyer:
-    portrayal["Color"] = color_buyer
-    portrayal["r"] = 0.9
-    portrayal["Layer"] = 1
+  portrayal["r"] = 0.5
+  portrayal["Layer"] = 2
+
+  # elif type(agent) is Buyer:
+  #   portrayal["Color"] = color_buyer
+  #   portrayal["r"] = 0.9
+  #   portrayal["Layer"] = 1
 
   return portrayal
 
 canvas_element = CanvasGrid(LFABM_portrayal, 20, 20, 500, 500)
-# chart_element = ChartModule([
-#   {"Label": "Sellers", "Color": color_seller},
-#   {"Label": "Wal-Mart", "Color": color_WM},
-#   {"Label": "Buyers", "Color": color_buyer}])
 chart_element = ChartModule([
   {"Label": "Sellers", "Color": color_seller}])
 
-server = ModularServer(main.Trade, [canvas_element, chart_element], modelname)
+server = ModularServer(Trade, [canvas_element, chart_element], modelname)
 server.launch()
